6e8b62266ef9c7984b67ed07a1e4ecf6d8734adb
andreicacipri <cipri9412@gmail.com>
2019-01-09 21:20:11 +0200

    create overloading

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -8,8 +8,12 @@ public class App {
     public static void main(String[] args) {
         Numbers firstNumbers = new Numbers();
         firstNumbers.calc(14, 17);
+        firstNumbers.calc(2.5,3.7);
         firstNumbers.multiply(14, 17);
-        firstNumbers.divide(14, 17);
+        firstNumbers.multiply(2.5, 3.7);
+        firstNumbers.divide(14.3, 3);
+        firstNumbers.divide(20, 2);
+        firstNumbers.average(14.5, 17);
         firstNumbers.average(14, 17);
     }
 }
--- src/main/java/org/fasttrackit/Numbers.java
+++ src/main/java/org/fasttrackit/Numbers.java
@@ -3,10 +3,20 @@ package org.fasttrackit;
 public class Numbers {
 
 
+    public int calc (int first, int second) {
+        int sum = first + second;
+        System.out.println("Sum is equal with  " + sum);
+        return sum;
+    }
     public double calc (double first, double second) {
         double sum = first + second;
         System.out.println("Sum is equal with  " + sum);
         return sum;
+    }
+    public int multiply (int first, int second) {
+        int multi = first * second;
+        System.out.println("Multiply is equal with " + multi);
+        return multi;
     }
     public double multiply (double first, double second) {
         double multi = first * second;
@@ -18,8 +28,18 @@ public class Numbers {
         System.out.println("Divide is equal with " + div);
         return div;
     }
+    public int divide (int first, int second) {
+        int div = first/second;
+        System.out.println("Divide is equal with " + div);
+        return div;
+    }
     public double average (double first, double second) {
         double ave = (first+second)/2;
+        System.out.println("Average is equal with " + ave);
+        return ave;
+    }
+    public int average (int first, int second) {
+        int ave = (first+second)/2;
         System.out.println("Average is equal with " + ave);
         return ave;
     }
